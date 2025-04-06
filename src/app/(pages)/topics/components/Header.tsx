1608bd31932bc505ea9c26f624f6c5b1a9a2f7a5
Facundo Barneche <fh.barneche@gmail.com>
2025-04-06 19:00:02 -0300

    [FIX] Header responsive

--- src/app/(pages)/topics/components/Header.tsx
+++ src/app/(pages)/topics/components/Header.tsx
@@ -1,19 +1,10 @@
 import { Button } from '@/app/components/ui/button';
-import { motion } from 'framer-motion';
 import { LogOut } from 'lucide-react';
 import Link from 'next/link';
 
 const Header = () => (
-  <header className="p-4 flex justify-between items-center">
-    <motion.h1
-      initial={{ opacity: 0, x: -20 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.5 }}
-      className="text-2xl font-bold text-white"
-    >
-      Elige un Tema
-    </motion.h1>
-    <div className="flex gap-2">
+  <header className="p-4 flex flex-col md:flex-row-reverse gap-4 justify-between">
+    <div className="flex justify-end">
       <Button variant="ghost" className="text-white hover:bg-white/20" asChild>
         <Link href="/profile">Perfil</Link>
       </Button>
@@ -21,10 +12,13 @@ const Header = () => (
         <Link href="/leaderboard">Ranking</Link>
       </Button>
       <Button variant="ghost" className="text-white hover:bg-white/20">
-        <LogOut className="h-4 w-4 mr-2" />
+        <LogOut className="h-4 w-4" />
         Salir
       </Button>
     </div>
+    <h1 className="text-2xl font-bold text-white whitespace-nowrap text-center md:justify-start">
+      Elige un Tema
+    </h1>
   </header>
 );
 
