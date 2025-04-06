333cef91e640c52bdea5fc3df7e020339a10b79f
Alan Guarino <aguarino@estudiantes.unsam.edu.ar>
2025-04-06 16:47:32 -0300

    [FEATURE] Complete design without functioinality

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import LoginForm from '@/app/components/forms/LoginForm';
 
+//Login page component
+
 export default function Login() {
-  return (
-    <LoginForm></LoginForm>
-  );
+  return <LoginForm></LoginForm>;
 }
