6e3e288a596badb99ac91982f409d061b6192415
Facundo Barneche <fh.barneche@gmail.com>
2025-04-08 20:47:18 -0300

    [FEATURE] Implementacion pantalla registro sin funcionalidad

---
+++ src/app/(pages)/register/components/Header.tsx
@@ -0,0 +1,15 @@
+import { CardDescription, CardHeader, CardTitle } from '@/app/components/ui/card';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <CardHeader className="space-y-1">
+      <CardTitle className="text-2xl font-bold text-center">Crear una cuenta</CardTitle>
+      <CardDescription className="text-center">
+        Únete a QuizMaster y pon a prueba tus conocimientos
+      </CardDescription>
+    </CardHeader>
+  );
+};
+
+export default Header;
