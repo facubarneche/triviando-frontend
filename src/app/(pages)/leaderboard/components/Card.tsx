75d470c9366f6e917c89db486ba8288116a435eb
Facundo Barneche <fh.barneche@gmail.com>
2025-05-07 13:03:42 -0300

    [FEATURE] Se agrega interface componente paginador

--- src/app/(pages)/leaderboard/components/Card.tsx
+++ src/app/(pages)/leaderboard/components/Card.tsx
@@ -11,7 +11,7 @@ import Table from './Table';
 
 const Card = () => {
   return (
-    <UICard className="bg-white mx-2 md:mx-10">
+    <UICard className="bg-white">
       <CardHeader>
         <CardTitle className="text-2xl text-center">Ranking</CardTitle>
         <CardDescription className="text-center">
@@ -19,7 +19,7 @@ const Card = () => {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Tabs defaultValue="weekly">
+        <Tabs defaultValue="allTime">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="weekly" disabled>
               Esta Semana
@@ -27,11 +27,11 @@ const Card = () => {
             <TabsTrigger value="allTime">Historico</TabsTrigger>
           </TabsList>
 
-          <TabsContent value="allTime">
-            <Table data={leaderboardDataMock.allTime} />
-          </TabsContent>
           <TabsContent value="weekly">
             <Table data={leaderboardDataMock.weekly} />
+          </TabsContent>
+          <TabsContent value="allTime">
+            <Table data={leaderboardDataMock.allTime} />
           </TabsContent>
         </Tabs>
       </CardContent>
--- src/app/(pages)/leaderboard/page.tsx
+++ src/app/(pages)/leaderboard/page.tsx
@@ -1,11 +1,15 @@
+import Paginator from '@/app/components/paginator';
 import Card from './components/Card';
 import Header from './components/Header';
 
 export default function Leaderboard() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-purple-500 to-indigo-700">
-      <Header />
-      <Card />
+      <div className="mx-2 md:mx-15">
+        <Header />
+        <Card />
+        <Paginator totalPages={10} />
+      </div>
     </div>
   );
 }
