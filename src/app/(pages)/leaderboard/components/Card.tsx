b169906b295d693dd30a54ebe99797c3bb400139
Facundo Barneche <fh.barneche@gmail.com>
2025-06-05 16:40:09 -0300

    [FEATURE] Ranking Semanal

--- src/app/(pages)/leaderboard/components/Card.tsx
+++ src/app/(pages)/leaderboard/components/Card.tsx
@@ -14,12 +14,16 @@ import { parseLeaderboardData } from '../utils/helpers';
 import ButtonPosition from './ButtonPosition';
 
 interface CardProps {
-  leaderBoardData: ILeaderBoardDTO;
+  weekly: ILeaderBoardDTO;
+  historical: ILeaderBoardDTO;
 }
 
-const Card = ({ leaderBoardData }: CardProps) => {
-  const { content, ...paginatorData } = leaderBoardData;
-  const historicalData = parseLeaderboardData(content);
+const Card = ({ weekly, historical }: CardProps) => {
+  const { content: historicalContent, ...historicalPagination } = historical;
+  const { content: weeklyContent, ...weeklyPagination } = weekly;
+  console.log('weekly', weekly);
+  const historicalData = parseLeaderboardData(historicalContent);
+  const weeklyData = parseLeaderboardData(weeklyContent);
 
   return (
     <UICard className="bg-white">
@@ -33,7 +37,7 @@ const Card = ({ leaderBoardData }: CardProps) => {
       <CardContent>
         <Tabs defaultValue="allTime">
           <TabsList className="grid w-full grid-cols-2 cursor-pointer">
-            <TabsTrigger value="weekly" className="cursor-pointer" disabled>
+            <TabsTrigger value="weekly" className="cursor-pointer">
               Esta Semana
             </TabsTrigger>
             <TabsTrigger value="allTime" className="cursor-pointer">
@@ -42,14 +46,15 @@ const Card = ({ leaderBoardData }: CardProps) => {
           </TabsList>
 
           <TabsContent value="weekly">
-            <Table data={leaderboardDataMock.weekly} />
+            <Table data={weeklyData} />
+            <Paginator {...weeklyPagination} />
           </TabsContent>
           <TabsContent value="allTime">
             <Table data={historicalData} />
+            <Paginator {...historicalPagination} />
           </TabsContent>
         </Tabs>
       </CardContent>
-      <Paginator {...paginatorData} />
     </UICard>
   );
 };
--- src/app/services/leaderBoardService.ts
+++ src/app/services/leaderBoardService.ts
@@ -3,6 +3,12 @@ import { BaseService } from './baseService';
 class LeaderBoardService extends BaseService {
   private readonly getUserPosition = (userId?: number) => (userId ? `/${userId}` : '');
 
+  getWeeklyRanking = async ({ userId, page }: { userId?: number; page: number }) => {
+    //TODO: Agregar el getUserPosition al endpoint
+    const { data } = await this.axiosService.get(`/users/ranking-semanal`, { params: { page } });
+    return data;
+  };
+
   getHistoricalRanking = async ({ userId, page }: { userId?: number; page: number }) => {
     const { data } = await this.axiosService.get(`/users/ranking${this.getUserPosition(userId)}`, {
       params: { page },
