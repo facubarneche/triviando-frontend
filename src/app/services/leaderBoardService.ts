8a9e0cc4a77084216d1eaed231f545c2324077cf
Facundo Barneche <fh.barneche@gmail.com>
2025-06-11 11:05:31 -0300

    [FIX] Boton mi posicion

--- src/app/services/leaderBoardService.ts
+++ src/app/services/leaderBoardService.ts
@@ -3,9 +3,11 @@ import { BaseService } from './baseService';
 class LeaderBoardService extends BaseService {
   private readonly getUserPosition = (userId?: number) => (userId ? `/${userId}` : '');
 
-  getWeeklyRanking = async ({ page }: { userId?: number; page: number }) => {
-    //TODO: Agregar el getUserPosition al endpoint
-    const { data } = await this.axiosService.get(`/users/ranking-semanal`, { params: { page } });
+  getWeeklyRanking = async ({ userId, page }: { userId?: number; page: number }) => {
+    const { data } = await this.axiosService.get(
+      `/users/ranking-semanal${this.getUserPosition(userId)}`,
+      { params: { page } },
+    );
     return data;
   };
 
